fe10feb6649b1490e4aedb494e475beda1a981da
sergio <sergio.rios@payco.co>
2023-04-26 12:41:27 -0500

    add template for controller lucky number

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,22 +2,28 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
 
    /**
     * @Route("/lucky/number")
     */
-    public function number(): Response
+    public function number(LoggerInterface $logger): Response
     {
+        $logger->info('I am using a service, in this case logger');
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 
 }
